1a565422deef17609de3a66ef9b71bb06593fde6
lauraz-15 <laura.zvirbule90@gmail.com>
2022-06-25 15:53:42 +0000

    Copy over js code to categories and search API pages

--- api/static/api/js/categories.js
+++ api/static/api/js/categories.js
@@ -1,5 +1,57 @@
-function textingTwo() {
-    console.log("Main js file is connected from the search.js page!")
+
+// -----------------------CATEGORIES / INSPIRATION API--------------------------------
+
+window.onload = function() {
+    getMultipleAPIcalls()
 }
 
-textingTwo();
+
+function dipsplayCategoryResults(searchCategory, categorie) {
+        searchCategory.hits.map(recipe=> {
+        var recipeCard = document.createElement('div');
+        recipeCard.classList.add('col-md-4')
+        recipeCard.innerHTML = 
+              `  <a href="${recipe.recipe.url}" target="_blank">
+                    <div class="card mb-4">
+                        <div class="card-body">
+                            <div class="image-container">
+                                <img class="card-image"
+                                src="${recipe.recipe.image}">
+                            </div>
+                            <h2 class="card-title">${recipe.recipe.label}</h2>
+                            <hr />
+                            <p class="card-bottom">Created by: ${recipe.recipe.source}     <span> Cal: ${recipe.recipe.calories.toFixed(0)}</span></p>
+
+                        </div>
+                    </div>
+                </div>
+                </a>
+    `
+    document.getElementsByClassName(`${categorie}-results`)[0].appendChild(recipeCard);
+
+})
+}
+
+// MULTIPLE API CALLS FOR CATEGORIES:
+
+async function getMultipleAPIcalls(recipeCard) {
+    const categories = ["healthy", "vegetarian", "deserts", "light", "grilled", "meat", "fish", "kids"];
+    const responses = await Promise.all(
+	categories.map(async category => {
+        let APP_ID = "2f1133a1"
+        let API_KEY = "63872c307c1ea5bea9706e007963df6f"
+        let response = await fetch(
+            `https://api.edamam.com/search?app_id=${APP_ID}&app_key=${API_KEY}&q=${category}`
+           
+        );
+        // console.log(response)
+        let searchCategory = await response.json()
+        console.log(searchCategory);
+        dipsplayCategoryResults(searchCategory, category)
+        // console.log(searchCategory.hits)
+        
+	})
+);
+}
+
+
--- api/static/api/js/search.js
+++ api/static/api/js/search.js
@@ -1,7 +1,56 @@
 
+// -----------------------SEARCH AREA API--------------------------------
 
-function textingOne() {
-    console.log("Main js file is connected from the search.js page!")
+function searchBtnClicked() {
+    let searchBtn = document.getElementById('search-btn')
+    searchBtn.addEventListener("click", ()=> {
+        sendAPIcall();
+        dipsplayResults(searchData)
+        })
 }
 
-textingOne();
+searchBtnClicked()
+
+async function sendAPIcall() {
+
+        let searchvalue = document.getElementById('text-value').value
+
+        let APP_ID = "2f1133a1"
+        let API_KEY = "63872c307c1ea5bea9706e007963df6f"
+        let response = await fetch(`https://api.edamam.com/search?app_id=${APP_ID}&app_key=${API_KEY}&q=${searchvalue}`);
+        console.log(response)
+        let searchData = await response.json()
+        console.log(searchData);
+        console.log(`${searchData.hits[0].recipe.label}`)
+        dipsplayResults(searchData)
+        console.log(`Search value is: ${searchvalue}`)
+        console.log(searchData.hits)
+    }
+
+    function dipsplayResults(searchData) {
+        searchData.hits.map(recipe=> {
+        var recipeCard = document.createElement('div');
+        recipeCard.classList.add('col-md-4')
+        recipeCard.innerHTML = 
+              `  <a href="${recipe.recipe.url}" target="_blank">
+                    <div class="card mb-4">
+                        <div class="card-body">
+                            <div class="image-container">
+                                <img class="card-image"
+                                src="${recipe.recipe.image}">
+                            </div>
+                            <h2 class="card-title">${recipe.recipe.label}</h2>
+                            <hr />
+                            <p class="card-bottom">Created by: ${recipe.recipe.source}     <span> Cal: ${recipe.recipe.calories.toFixed(0)}</span></p>
+
+                        </div>
+                    </div>
+                </div>
+                </a>
+    `
+  document.getElementsByClassName('search-results')[0].appendChild(recipeCard);
+})
+ }
+
+
+
